e161ccd78c233251429deac060c9a4fbbf512bcb
tagf <tagf@users.noreply.github.com>
2016-03-04 02:06:36 +0300

    output updated

--- vocabreverse.cpp
+++ vocabreverse.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 int main() {
-      map <string, vector<string>> lateng;
-      int n;
-      cin >> n;
+    map <string, vector<string>> lateng;
+    int n;
+    cin >> n;
     for (int wc = 0; wc < n; ++wc) {
         string eng, dummy, lat;
         cin >> eng >> dummy;
-        
+
         bool cont = true;
         do {
             cin >> lat;
@@ -22,7 +22,7 @@ int main() {
             } else {
                 cont = false;
             }
-            
+
             if (lateng.find(lat) == lateng.end()) {
                 vector <string> temp(1);
                 temp[0] = eng;
@@ -31,11 +31,15 @@ int main() {
                 lateng[lat].push_back(eng);
             }
         } while (cont);
-        
     }
     
     for (auto lat : lateng) {
-        cout << lat.first << '\n';
+        cout << lat.first << " - ";
+        lat.second.sort();
+        for (int i = 0; i + 1 < lat.second.size(); ++i) {
+            cout << lat.second[i] << ", "
+        }
+        cout << lat.second[lat.second.size() - 1] << "\n"
     }
-      return 0;
+    return 0;
 }
